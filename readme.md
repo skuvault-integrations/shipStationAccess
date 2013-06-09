9e61008d8e60e5fcdc321428d1b2d697858105b6
Slav Ivanyuk <slav.isv@gmail.com>
2013-06-09 10:27:55 -0400

    Initial project commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+ShipStationAccess is .NET wrapper around [ShipStation API](http://api.shipstation.com/) was created to simplify and speed up working with it.
---
+++ src/ShipStationAccess/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[ assembly : AssemblyTitle( "ShipStationAccess" ) ]
+[ assembly : AssemblyConfiguration( "" ) ]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[ assembly : Guid( "c42ca23c-549e-4bf1-8403-3e85a2a02f2e" ) ]
