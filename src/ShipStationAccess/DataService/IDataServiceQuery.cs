1e3679781893d6ccc42d061964e2458cd3e71182
Slav Ivanyuk <slav.isv@gmail.com>
2013-06-21 15:27:19 -0400

    minor code cleanup

--- src/ShipStationAccess/DataService/IDataServiceQuery.cs
+++ src/ShipStationAccess/DataService/IDataServiceQuery.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShipStationAccess.DataService
 {
-	public interface IDataServiceQuery< TElement > : IQueryable< TElement >, IEnumerable< TElement >, IQueryable, IEnumerable
+	public interface IDataServiceQuery< TElement > : IQueryable< TElement >
 	{
 		IDataServiceQuery< TElement > Expand< TTarget >( Expression< Func< TElement, TTarget > > navigationPropertyAccessor );
 
