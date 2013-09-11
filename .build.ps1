7b3202327a8cbf58eaee5fe775233986c52ccdbb
Slav Ivanyuk <slav.isv@gmail.com>
2013-09-11 12:34:06 -0400

    updated packages and versions

--- .build.ps1
+++ .build.ps1
@@ -110,11 +110,11 @@ task NuGet Package, Version, {
 		<tags>ShipStation</tags>
 		<dependencies> 
 			<group targetFramework="net45">
-				<dependency id="Netco" version="1.1.0" />
-				<dependency id="Microsoft.Data.Edm" version="5.5.0" />
-				<dependency id="Microsoft.Data.OData" version="5.5.0" />
-				<dependency id="Microsoft.Data.Services.Client" version="5.5.0" />
-				<dependency id="System.Spatial" version="5.5.0" />
+				<dependency id="Netco" version="1.3.0" />
+				<dependency id="Microsoft.Data.Edm" version="5.6.0" />
+				<dependency id="Microsoft.Data.OData" version="5.6.0" />
+				<dependency id="Microsoft.Data.Services.Client" version="5.6.0" />
+				<dependency id="System.Spatial" version="5.6.0" />
 			</group>
 		</dependencies>
 	</metadata>
