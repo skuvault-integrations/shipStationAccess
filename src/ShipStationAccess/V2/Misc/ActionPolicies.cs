cbfe4cb385667ae8d94577f47bec3f4b148d1db6
Slav Ivanyuk <slav.isv@gmail.com>
2015-06-11 21:25:08 -0400

    updated retries to ignore 401 (unauthorized) errors and just pass exception to the caller

--- src/ShipStationAccess/V2/Misc/ActionPolicies.cs
+++ src/ShipStationAccess/V2/Misc/ActionPolicies.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Netco.ActionPolicyServices;
-using Netco.Logging;
 using Netco.Utils;
 
 namespace ShipStationAccess.V2.Misc
@@ -13,7 +13,16 @@ namespace ShipStationAccess.V2.Misc
 			get { return _shipStationSumbitPolicy; }
 		}
 
-		private static readonly ActionPolicy _shipStationSumbitPolicy = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
+		private static readonly ExceptionHandler _exceptionHandler = delegate( Exception x )
+		{
+			var webX = x as WebException;
+			if( webX == null )
+				return true;
+
+			return webX.Response.GetHttpStatusCode() != HttpStatusCode.Unauthorized;
+		};
+
+		private static readonly ActionPolicy _shipStationSumbitPolicy = ActionPolicy.With( _exceptionHandler ).Retry( 10, ( ex, i ) =>
 		{
 			ShipStationLogger.Log.Trace( ex, "Retrying ShipStation API submit call for the {retryCounter} time", i );
 			SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
@@ -24,7 +33,7 @@ namespace ShipStationAccess.V2.Misc
 			get { return _shipStationSumbitAsyncPolicy; }
 		}
 
-		private static readonly ActionPolicyAsync _shipStationSumbitAsyncPolicy = ActionPolicyAsync.Handle< Exception >().RetryAsync( 10, async ( ex, i ) =>
+		private static readonly ActionPolicyAsync _shipStationSumbitAsyncPolicy = ActionPolicyAsync.With( _exceptionHandler ).RetryAsync( 10, async ( ex, i ) =>
 		{
 			ShipStationLogger.Log.Trace( ex, "Retrying ShipStation API submit call for the {retryCounter} time", i );
 			await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) );
@@ -35,7 +44,7 @@ namespace ShipStationAccess.V2.Misc
 			get { return _shipStationGetPolicy; }
 		}
 
-		private static readonly ActionPolicy _shipStationGetPolicy = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
+		private static readonly ActionPolicy _shipStationGetPolicy = ActionPolicy.With( _exceptionHandler ).Retry( 10, ( ex, i ) =>
 		{
 			ShipStationLogger.Log.Trace( ex, "Retrying ShipStation API get call for the {retryCounter} time", i );
 			SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
@@ -46,7 +55,7 @@ namespace ShipStationAccess.V2.Misc
 			get { return _shipStationGetAsyncPolicy; }
 		}
 
-		private static readonly ActionPolicyAsync _shipStationGetAsyncPolicy = ActionPolicyAsync.Handle< Exception >().RetryAsync( 10, async ( ex, i ) =>
+		private static readonly ActionPolicyAsync _shipStationGetAsyncPolicy = ActionPolicyAsync.With( _exceptionHandler ).RetryAsync( 10, async ( ex, i ) =>
 		{
 			ShipStationLogger.Log.Trace( ex, "Retrying ShipStation API get call for the {retryCounter} time", i );
 			await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) );
