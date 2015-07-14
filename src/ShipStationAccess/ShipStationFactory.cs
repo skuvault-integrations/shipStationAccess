c249fb2c345fd3b45e66642ae620a852cbca0199
deltasem <deltasem@gmail.com>
2015-07-14 21:48:45 +0300

    V1 removed

--- src/ShipStationAccess/ShipStationFactory.cs
+++ src/ShipStationAccess/ShipStationFactory.cs
@@ -4,19 +4,11 @@ namespace ShipStationAccess
 {
 	public interface IShipStationFactory
 	{
-		V1.IShipStationService CreateServiceV1( V1.Models.ShipStationCredentials credentials );
 		V2.IShipStationService CreateServiceV2( V2.Models.ShipStationCredentials credentials );
 	}
 
 	public class ShipStationFactory : IShipStationFactory
 	{
-		public V1.IShipStationService CreateServiceV1( V1.Models.ShipStationCredentials credentials )
-		{
-			Condition.Requires( credentials, "credentials" ).IsNotNull();
-
-			return new V1.ShipStationService( credentials );
-		}
-
 		public V2.IShipStationService CreateServiceV2( V2.Models.ShipStationCredentials credentials )
 		{
 			Condition.Requires( credentials, "credentials" ).IsNotNull();
