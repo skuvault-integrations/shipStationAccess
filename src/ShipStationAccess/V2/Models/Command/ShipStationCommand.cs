a54c9f01c41362b89477bb4fea27f178602ee9c9
Jason Henson <jason.henson@skuvault.com>
2015-10-07 10:46:38 -0400

    remove accidental commit

--- src/ShipStationAccess/V2/Models/Command/ShipStationCommand.cs
+++ src/ShipStationAccess/V2/Models/Command/ShipStationCommand.cs
@@ -5,7 +5,6 @@
 		public static readonly ShipStationCommand Unknown = new ShipStationCommand( string.Empty );
 		public static readonly ShipStationCommand GetOrders = new ShipStationCommand( "/Orders/List" );
 		public static readonly ShipStationCommand GetTags = new ShipStationCommand( "/Accounts/ListTags" );
-		public static readonly ShipStationCommand GetLabel = new ShipStationCommand( "/Accounts/ListTags" );
 		public static readonly ShipStationCommand CreateUpdateOrder = new ShipStationCommand( "/Orders/CreateOrder" );
 		public static readonly ShipStationCommand UpdateOrderItemsWarehouseLocation = new ShipStationCommand( "/Orders/UpdateWarehouseLocation" );
 		public static readonly ShipStationCommand GetStores = new ShipStationCommand( "/Stores" );
