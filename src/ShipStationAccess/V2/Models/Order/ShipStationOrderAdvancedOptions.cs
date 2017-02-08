8a4edbc7f36b04bde56dc57846c1210ad29b79f1
Sergey Shumakov <sergey.shumakov@skuvault.com>
2017-02-08 11:00:14 +0300

    SI-769 Add MergedIds field to AdvancedOptions model

--- src/ShipStationAccess/V2/Models/Order/ShipStationOrderAdvancedOptions.cs
+++ src/ShipStationAccess/V2/Models/Order/ShipStationOrderAdvancedOptions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace ShipStationAccess.V2.Models.Order
 {
@@ -31,6 +32,12 @@ namespace ShipStationAccess.V2.Models.Order
 
 		[ DataMember( Name = "source" ) ]
 		public string Source{ get; set; }
+
+		[ DataMember( Name = "mergedOrSplit" ) ]
+		public bool MergedOrSplit{ get; set; }
+
+		[ DataMember( Name = "mergedIds" ) ]
+		public IList< long > MergedIds{ get; set; }
 
 		[ DataMember( Name = "billToParty" ) ]
 		public string BillToParty{ get; set; }
