0f4322a008c847911dcfee618768480ae4f44388
Victoria <victoria.felice@skuvault.com>
2023-05-03 14:11:50 -0400

    GUARD-2926: increment version

--- src/Global/GlobalAssemblyInfo.cs
+++ src/Global/GlobalAssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 // Keep in track with CA API version
-[ assembly : AssemblyVersion( "1.5.0.0" ) ]
+[ assembly : AssemblyVersion( "1.5.0.1-alpha" ) ]
